3af78834c23ef2c2989507c4f66baa8215f504b9
techi9 <tucha.989@gmail.com>
2021-11-14 16:15:05 +0300

    added sidebar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
+import Link from "next/link";
+import sidebar from "../styles/sidebar.module.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <div>
+
+        <div className={sidebar.sidebar}>
+          <Link href={"/"}>
+            <a >Введение в теорему</a>
+          </Link>
+          <Link href={"/"}>
+            <a >Критичесский граф</a>
+          </Link>
+          <Link href={"/"}>
+            <a >Дальше идут вроде как игры</a>
+          </Link>
+
+
+        </div>
+        <Component {...pageProps} />
+      </div>
+  )
 }
 
 export default MyApp
