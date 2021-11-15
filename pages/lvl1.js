9fff831330d6f260ad2c8b0c544bfab626676993
brokkko <nsstrannikova@mail.ru>
2021-11-15 20:43:18 +0300

    added intro page

---
+++ pages/lvl1.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+    let x = 15
+    x = new Date()
+
+
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <meta name="description" content="Generated create next app" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {/*----------------------------------------------------------------*/}
+            <main className={styles.main}>
+
+                <div className={styles.title}>
+                    <h1 align={"left"}>
+                        <font size={+6} color={"#808080"} FACE={"century schoolbook"}>
+                            <strong>Рамсеевская теория графов</strong>
+                        </font>
+
+                    </h1>
+                    <hr noshade width="98%" align="left">
+                    </hr>
+                </div>
+
+                <body>
+
+                </body>
+
+
+            </main>
+            {/*-----------------------------------------------------------------*/}
+            <footer className={styles.footer}>
+
+
+
+            </footer>
+        </div>
+    )
+}
