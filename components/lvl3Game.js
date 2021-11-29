74aec0e8b6cf901d4234a287ed274687b4d19fae
brokkko <nsstrannikova@mail.ru>
2021-11-29 17:11:04 +0300

    changes by brokkko

---
+++ components/lvl3Game.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Vertex from "./Vertex";
+import Line from "./Line";
+import styles from '../styles/colorSelector.module.css'
+import graphGeneration from '/utility/graphGeneration'
+
+class Lvl3Game extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+
+        return (
+            <div>
+
+            </div>
+
+        );
+    }
+}
+
+export default Lvl3Game
