c25b7095daa01dacf0c9f2c540a759903674a0fa
techi9 <53077251+techi9@users.noreply.github.com>
2021-12-26 03:00:48 +0300

    Update Vertex.js

--- components/Vertex.js
+++ components/Vertex.js
@@ -1,24 +1,6 @@
 import React, {Component} from "react";
 
 class Vertex extends React.Component {
-    state = {
-
-    };
-    handleClick1 = () => {
-        this.setState({
-            color1: "black"
-        });
-    };
-    handleClick2 = () => {
-        this.setState({
-            color2: "black"
-        });
-    };
-    handleClick3 = () => {
-        this.setState({
-            color3: "black"
-        });
-    };
     render() {
         return (
             <circle cx ={this.props.x} cy={this.props.y} r = "1" />
